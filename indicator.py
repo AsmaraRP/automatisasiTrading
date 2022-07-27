f9acef4e63005786887eacec925e4f72a7e408b3
Rifang <rifang.asmara12@gmail.com>
2022-07-27 17:38:03 +0700

    add moving average

--- indicator.py
+++ indicator.py
@@ -1,37 +1,36 @@
+import pandas_datareader.data as reader
+import datetime as dt
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import pandas as pd
-import sqlalchemy
-from binance.client import Client
-from binance import BinanceSocketManager
-import api_key from "./binance_apiKey.ipynb"
-import api_secret from "./binance_apiKey.ipynb"
-# Get data BTC dari Big Data
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
-# Get BTC dari Binance
-client = Client(api_key, api_secret)
-bsm = BinanceSocketManager(client)
-socket = bsm.trade_socket('BTCUSDT')
-
-while True:
-    await socket._aenter_()
-    msg = await socket.recv()
-    frame = createframe(msg)
-    frame.to_sql('BTCUSDT', engine, if_exists='append', index=False)
-    print frame
-
-
-def createframe(msg):
-    df = pd.DataFrame([msg])
-    df = df.loc[:, ['s', 'E', 'p']]
-    df.columns = ['symbol', 'Time', 'Price']
-    df.Price = df.Price.astype(float)
-    df.Time - pd.to_datetime(df.Time, unit='ms')
-    return df
+# Get BTC
+start2 = dt.date(2022, 3, 18)
+end2 = dt.date(2022, 4, 20)
+crypto = ['BTC-USD']
+df_b = reader.get_data_yahoo(crypto, start2, end2)
+# df_b.drop('High', inplace=True, axis=1)
+# df_b.drop('Open', inplace=True, axis=1)
+# df_b.drop('Low', inplace=True, axis=1)
+# df_b.drop('Adj Close', inplace=True, axis=1)
+# df_b["Harga"] = df_b["Close"].values
+df_b
 
 # VWAP
+
 # Moving Average
-# Moving Average 30, 90, 200
+df_b['MA30'] = df_b['Close'].rolling(30).mean()
+df_b['MA90'] = df_b['Close'].rolling(90).mean()
+# remove data
+df_b = df_b.dropna()
+df_b.drop('Regresi', inplace=True, axis=1)
+df_b.drop('Regresi-', inplace=True, axis=1)
+df_b.drop('Regresi+', inplace=True, axis=1)
+df_b.drop('test', inplace=True, axis=1)
+df_b
+
 # MA cross
 # golden cros dan death cross
 # RSI
